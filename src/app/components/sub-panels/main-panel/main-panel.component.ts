1f9182f463d2e9c670975bb06b18ea49dc5b8339
vlb3rt <alb.strozynski@gmail.com>
2024-01-19 18:51:50 +0100

    Small Css updates

--- src/app/components/sub-panels/main-panel/main-panel.component.ts
+++ src/app/components/sub-panels/main-panel/main-panel.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-main-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './main-panel.component.html',
   styleUrl: './main-panel.component.css'
 })
